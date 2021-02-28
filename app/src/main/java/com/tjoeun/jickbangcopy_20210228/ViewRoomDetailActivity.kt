6b615a5ba634b55e32d20cf3c72d97d154eab5c0
seoyeongkang <jumprope123@naver.com>
2021-02-28 15:12:17 +0900

    받아온 방 정보로 => 상세화면 데이터 출력

--- app/src/main/java/com/tjoeun/jickbangcopy_20210228/ViewRoomDetailActivity.kt
+++ app/src/main/java/com/tjoeun/jickbangcopy_20210228/ViewRoomDetailActivity.kt
@@ -3,6 +3,7 @@ package com.tjoeun.jickbangcopy_20210228
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.tjoeun.jickbangcopy_20210228.datas.Room
+import kotlinx.android.synthetic.main.activity_view_room_detail.*
 
 class ViewRoomDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +12,11 @@ class ViewRoomDetailActivity : AppCompatActivity() {
 
         val roomData = intent.getSerializableExtra("roomInfo") as Room
 
+        priceTxt.text = roomData.getFormatedPrice()
+        descriptionTxt.text = roomData.description
 
+        address.text = roomData.address
+        floorTxt.text = roomData.getFormatedFloor()
 
     }
 }
