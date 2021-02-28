305e8071f5c8f711baf5c900117949f621b7919e
seoyeongkang <jumprope123@naver.com>
2021-02-28 13:34:25 +0900

    실제 설명 문구만 우선 반영

--- app/src/main/java/com/tjoeun/jickbangcopy_20210228/adapters/RoomAdapter.kt
+++ app/src/main/java/com/tjoeun/jickbangcopy_20210228/adapters/RoomAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.tjoeun.jickbangcopy_20210228.R
 import com.tjoeun.jickbangcopy_20210228.datas.Room
 import java.util.ArrayList
@@ -24,6 +25,19 @@ class RoomAdapter(
         }
 
         val row = tempRow!!
+
+//        row
+        val data = mList[position]
+//        mRoomList.add(Room(8000, "서울시 종로구", 4, "종로의 4층 방"))
+
+        //?사진어캄
+        val priceTxt = row.findViewById<TextView>(R.id.priceTxt)
+        val addressTxt = row.findViewById<TextView>(R.id.addressAndFloorTxt)
+        val descriptionTxt = row.findViewById<TextView>(R.id.descriptionTxt)
+
+//        설명 문구는 : 있는 그대로
+
+        descriptionTxt.text = data.description
 
         return row
     }
