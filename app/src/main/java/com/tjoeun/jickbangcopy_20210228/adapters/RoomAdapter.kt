0d500e272629309b3d4a2b912dd2983f5b8e398d
seoyeongkang <jumprope123@naver.com>
2021-02-28 12:25:50 +0900

    어댑터 뼈대 작업(2) - getView 내부 초안 완성

--- app/src/main/java/com/tjoeun/jickbangcopy_20210228/adapters/RoomAdapter.kt
+++ app/src/main/java/com/tjoeun/jickbangcopy_20210228/adapters/RoomAdapter.kt
@@ -1,13 +1,30 @@
 package com.tjoeun.jickbangcopy_20210228.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.tjoeun.jickbangcopy_20210228.R
 import com.tjoeun.jickbangcopy_20210228.datas.Room
 import java.util.ArrayList
 
-class RoomAdapter(val mContext : Context,
-                  val resId : Int,
-                  val mList: ArrayList<Room>) : ArrayAdapter<Room>(mContext, resId, mList) {
+class RoomAdapter(
+    val mContext : Context,
+    val resId : Int,
+    val mList: ArrayList<Room>) : ArrayAdapter<Room>(mContext, resId, mList) {
 
+    val inf = LayoutInflater.from(mContext)
 
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if(tempRow ==  null ){
+            tempRow = inf.inflate(R.layout.room_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        return row
+    }
 }
