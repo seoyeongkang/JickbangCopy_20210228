dc6a9b008efc1f15668c0f0972121ab36338c5fe
seoyeongkang <jumprope123@naver.com>
2021-02-28 16:47:34 +0900

    앱의 시작점 변경 - 로딩화면 추가

---
+++ app/src/main/java/com/tjoeun/jickbangcopy_20210228/splashActivity.kt
@@ -0,0 +1,11 @@
+package com.tjoeun.jickbangcopy_20210228
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class splashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
